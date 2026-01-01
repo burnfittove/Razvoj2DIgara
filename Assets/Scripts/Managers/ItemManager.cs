f36ca2d93dea72a9bed93facf9fadfd72243c89a
Ema <ian.dobrincic@gmail.com>
2026-01-01 17:10:03 +0100

    ItemManager Changes

--- Assets/Scripts/Managers/ItemManager.cs
+++ Assets/Scripts/Managers/ItemManager.cs
@@ -1,4 +1,8 @@
+using System;
+using System.Collections.Generic;
+using Events;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using Random = UnityEngine.Random;
 
 namespace Managers
@@ -9,22 +13,36 @@ namespace Managers
         [SerializeField] private GameObject[] regularPool;
         [SerializeField] private GameObject[] shopPool;
         [SerializeField] private GameObject[] demonPool;
+        [SerializeField] private Transform itemSpawnPosition;
+        public ItemPool debugPool;
 
         private void Awake()
         {
             allItems[(int)ItemPool.RegularPool] = regularPool;
             allItems[(int)ItemPool.ShopPool] = shopPool;
             allItems[(int)ItemPool.DemonPool] = demonPool;
+
+            GameEventManager.Instance.itemEvents.OnCreateItemFromPool += GetItemFromPool;
         }
 
-        public GameObject GetItemFromPool(ItemPool itemPool)
+        private void GetItemFromPool(ItemPool itemPool)
         {
             // Get item pool
             ref var pool = ref allItems[(int)itemPool];
             
             // Get item from pool
-            var itemId = Random.Range(0, pool.Length);
-            return pool[itemId] ? pool[itemId] : pool[0];
+            var itemId = 0;
+            
+            // Try to get an item and if the item is null (it has been pulled previously) try again.
+            do
+            {
+                itemId = Random.Range(0, pool.Length);
+            } while (!pool[itemId]);
+            
+            Instantiate(pool[itemId], itemSpawnPosition.position, Quaternion.identity);
+            
+            // Set the item to null if it has been pulled unless it's the first item (default fallback)
+            if (itemId != 0) pool[itemId] = null;
         }
         
         public GameObject GetItem()
