85e82bd0b2d3a9a078df36f1241b991c7acec8cf
Ana <ian.dobrincic@gmail.com>
2026-02-20 11:40:41 +0100

    New Rooms
    - Added a few new regular rooms
    - Added a new enemy; Green Wizard

---
+++ Assets/Prefabs/Enemies/Far-range/ShadowWizardMoneyGang/Green/States/GreenWizardEnemyStateController.cs
@@ -0,0 +1,10 @@
+using Enemies.States;
+using UnityEngine;
+
+namespace Prefabs.Enemies.Far_range.ShadowWizardMoneyGang.Green.States
+{
+    public class GreenWizardEnemyStateController : ShooterEnemyStateController
+    {
+        
+    }
+}
